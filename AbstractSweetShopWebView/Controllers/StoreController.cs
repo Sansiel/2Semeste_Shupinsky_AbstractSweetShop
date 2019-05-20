5ca09e86a9268c6d713ee7e050c161b1950f56c5
Sansiel <deviler.san@yandex.ru>
2019-05-20 13:41:22 +0400

    StoreControler

---
+++ AbstractSweetShopWebView/Controllers/StoreController.cs
@@ -0,0 +1,64 @@
+﻿using AbstractSweetShopServiceDAL.BindingModels;
+using AbstractSweetShopServiceDAL.Interfaces;
+using System.Web.Mvc;
+
+namespace AbstractSweetShopWebView.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly IStoreService _service;
+
+        public StoreController(IStoreService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_service.GetList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePost()
+        {
+            _service.AddElement(new StoreBindingModel
+            {
+                StoreName = Request["StoreName"]
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var viewModel = _service.GetElement(id);
+            var bindingModel = new StoreBindingModel
+            {
+                Id = id,
+                StoreName = viewModel.StoreName
+            };
+            return View(bindingModel);
+        }
+
+        [HttpPost]
+        public ActionResult EditPost()
+        {
+            _service.UpdElement(new StoreBindingModel
+            {
+                Id = int.Parse(Request["Id"]),
+                StoreName = Request["StoreName"]
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _service.DelElement(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
